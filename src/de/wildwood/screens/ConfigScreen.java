c26cb8b60a23a944e9587d374ad5a5f1155c0770
SBSJavatar <SBSJavatar@users.noreply.github.com>
2019-07-04 15:09:19 +0200

    new method of menu layout

--- src/de/wildwood/screens/ConfigScreen.java
+++ src/de/wildwood/screens/ConfigScreen.java
@@ -6,12 +6,16 @@ import java.math.MathContext;
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.gui.HorizontalSlider;
 import de.gurkenlabs.litiengine.gui.ImageComponent;
+import de.gurkenlabs.litiengine.gui.Menu;
 import de.gurkenlabs.litiengine.gui.NumberAdjuster;
 import de.gurkenlabs.litiengine.gui.TextFieldComponent;
 import de.gurkenlabs.litiengine.gui.screens.Screen;;
 
 public class ConfigScreen extends Screen {
 	public static final String NAME = "CONFIGSCREEN";
+
+	de.gurkenlabs.litiengine.gui.Menu menu;
+	de.gurkenlabs.litiengine.gui.ImageComponentList imgcomplist;
 
 	TextFieldComponent maxfps;
 	NumberAdjuster maxfps_value;
@@ -23,7 +27,15 @@ public class ConfigScreen extends Screen {
 
 	@Override
 	protected void initializeComponents() {
-		this.maxfps = new TextFieldComponent(100, 100, 100, 50, null, "maxfps");
+		double menuPosX = Game.window().getWidth() / 4;
+		double menuPosY = Game.window().getHeight() / 10;
+
+		this.menu = new Menu(menuPosX, menuPosY, menuPosX * 2, menuPosY * 8, "FPS", "QUALITÄT", "...");
+		menu.onClicked(callback -> {
+			menu.getCurrentSelection();
+		});
+
+		/*this.maxfps = new TextFieldComponent(100, 100, 100, 50, null, "maxfps");
 		this.maxfps_value = new NumberAdjuster(200, 100, 100, 50, null, null, 10, 300,
 				Game.config().client().getMaxFps(), 5);
 		this.maxfps_value.onValueChange(consumer -> {
@@ -33,13 +45,16 @@ public class ConfigScreen extends Screen {
 			this.maxfps_value.setCurrentValue(new BigDecimal(new BigDecimal(consumer).intValue()));
 			Game.config().client().setMaxFps(new BigDecimal(consumer).intValue());
 		});
-
+		*/
 		// this.maxfps_setting = new HorizontalSlider(400, 100, 400, 50, 25, 240, 5);
 		// this.maxfps_setting.onChange(consumer ->
 		// maxfps_value.setText(Float.toString(maxfps_setting.getCurrentValue())));
 
-		this.getComponents().add(maxfps);
-		this.getComponents().add(maxfps_value);
+		this.getComponents().add(menu);
+
+		// this.getComponents().add(maxfps);
+		// this.getComponents().add(maxfps_value);
+
 		// this.getComponents().add(maxfps_setting);
 
 	}
